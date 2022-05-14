0d64485226cbfed889e31391e3faa49f0b10fe46
hseyinsams <huseyinsamsun81@gmail.com>
2022-05-14 10:45:41 +0300

    add

--- ESourcing.Products/Program.cs
+++ ESourcing.Products/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddTransient<IProductContext, ProductContext>();
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
-
+//
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
